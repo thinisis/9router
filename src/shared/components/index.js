1e8f32171f2bb17c86e0d8a00a26e551dca7b3e2
thinisis <24206254+thinisis@users.noreply.github.com>
2026-07-17 07:05:52 +0000

    fix(ui): export ProviderSection to fix React #130 crash
    Providers page imported ProviderSection from the shared barrel but it was
    not re-exported, so React rendered undefined (minified error #130). Also
    serve /favicon.ico to stop 404 noise.

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -37,6 +37,7 @@ export { default as SegmentedControl } from "./SegmentedControl";
 export { default as Tooltip } from "./Tooltip";
 export { default as ProviderInfoCard } from "./ProviderInfoCard";
 export { default as CapacityBadges } from "./CapacityBadges";
+export { default as ProviderSection } from "./ProviderSection";
 export { default as TextArea } from "./TextArea";
 export { default as EmptyState } from "./EmptyState";
 export { default as GlassAlert } from "./GlassAlert";
